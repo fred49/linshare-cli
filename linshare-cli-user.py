d698428345505d66b1a50c003ca940aef3f4b70e
Frédéric MARTIN <frederic.martin.fma@gmail.com>
2013-09-29 23:39:47 +0200

    List received shares command was added. Refactoring and cleaning code.

--- linshare-cli-user.py
+++ linshare-cli-user.py
@@ -100,7 +100,6 @@ class MyPref(PrefFile):
 		#c = type('CPref' , (object,) , dict())
 		c = CPref(self.prefix)
 		log = logging.getLogger('linshare-cli')
-		log.error('pouet')
 
 		if os.path.isfile( self.file ) :
 			fileParser = ConfigParser.ConfigParser()
@@ -129,20 +128,22 @@ class MyPref(PrefFile):
 
 class Config(object):
 	""" Class design to store all the configuration for this application."""
+	section="server"
+	filepath="~/.linshare.ini"
 	def __init__(self):
 		# Ldap parameters
 		self.fields = ['host', 'realm', 'user', 'password', 'application_name', 'config', 'server_section', 'debug' ] 
-		myPref = MyPref("~/.linshare.ini", 'server', expanduser=True, prefix = "\t", mandatory = False)
+		myPref = MyPref(self.filepath, self.section, expanduser=True, prefix = "\t", mandatory = False)
 		self.linshare = myPref(self.fields)
 		self.decode_password()
 
 	def reload(self, args):
 		if args.server_section or args.config :
-			server_section_name = 'server'
+			server_section_name = self.section
 			if args.server_section :
 				server_section_name += '-' + str(args.server_section)
 		
-			myPref = MyPref("~/.linshare.ini", server_section_name, expanduser=True, prefix = "\t", mandatory = False)
+			myPref = MyPref(self.filepath, server_section_name, expanduser=True, prefix = "\t", mandatory = False)
 			self.linshare = myPref(self.fields)
 			self.decode_password()
 
@@ -156,7 +157,20 @@ class Config(object):
 			else :
 				# field not set, setting on using the config file value
 				setattr(args, field, getattr(self.linshare, field, None))
-	
+
+	def write_default_config_file(self):
+
+		log = logging.getLogger('linshare-cli')
+		filepath = os.path.expanduser(self.filepath)
+		if os.path.isfile(filepath) :
+			log.error("Can not generate the pref file because it already exists. : " + filepath)
+			return 1
+		else :
+			with open(filepath, 'w') as f:
+				f.write("[" + self.section + "]\n")
+				for field in self.fields:
+					f.write(field + "=\n")
+
 	def decode_password(self):
 		try:
 			if self.linshare.password :
@@ -189,7 +203,6 @@ class Config(object):
 		return res
 
 
-# ---------------------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------------------
 log = logging.getLogger()
@@ -211,7 +224,57 @@ if True :
 log = logging.getLogger('linshare-cli')
 
 # ---------------------------------------------------------------------------------------------------------------------
+
+
+
+def extract_file_name(content_dispo):
+	file_name = ""
+	for key_val in content_dispo.split(';') :
+		param = key_val.strip().split('=')
+		if param[0] == "filename":
+			file_name =  param[1]
+		if param[0] == "filename*":
+			file_name =  param[1].split("'")[2]
+	return file_name
+
+
+class file_with_callback(file):
+	def __init__(self, path, mode, callback, size=None, *args):
+		file.__init__(self, path, mode)
+		if size :
+			self._total = size
+		else:
+			self.seek(0, os.SEEK_END)
+			self._total = self.tell()
+		self.seek(0)
+		self._callback = callback
+		self._args = args
+		self._seen = 0.0
+
+	def __len__(self):
+		return self._total
+
+	def read(self, size):
+		data = file.read(self, size)
+		self._seen += size
+		if self._seen > self._total :
+			self._callback(self._total)
+		else:
+			self._callback(self._seen)
+		return data
+
+	def write(self, data):
+		if data :
+			self._seen += len(data)
+		if self._seen > self._total :
+			self._callback(self._total)
+		else:
+			self._callback(self._seen)
+		data = file.write(self, data)
+
+# ---------------------------------------------------------------------------------------------------------------------
 class DefaultCommand(object):
+	fragment_auth = "/rest/authentication/authorized"
 
         def __init__(self):
 		self.log = logging.getLogger('linshare-cli' + "." + str(self.__class__.__name__.lower()))
@@ -247,18 +310,6 @@ class DefaultCommand(object):
 		self.password = password
 		self.root_url = args.host + "/" + args.application_name + "/" + "webservice/rest/"
 
-
-		#self.fragment_upload = "documents/upload"
-		#if self.fragment_upload[-1] != "/" :
-		#	self.fragment_upload += "/" 
-		self.fragment_upload = "documents/upload"
-		self.fragment_upload = "documents"
-		self.fragment_list = "documents"
-		self.fragment_download = "documents/%s/download"
-
-		# /rest/authentication/authorized
-
-
 		# We declare all the handlers useful here.
 		debuglevel = 0
 		if self.debug :
@@ -286,11 +337,10 @@ class DefaultCommand(object):
 	def add_auth_header(self, request ):
 		base64string = base64.encodestring('%s:%s' % (self.user, self.password)).replace('\n', '')
 		request.add_header("Authorization", "Basic %s" % base64string)
-		#request.add_header("Cookie", "JSESSIONID=3AF1BB555CAFFEC7A3CA11DE805BB031")
+		#request.add_header("Cookie", "JSESSIONID=")
 
 
-
-
+# ---------------------------------------------------------------------------------------------------------------------
 class TestCommand(DefaultCommand):
 
 	def __call__(self, args):
@@ -298,54 +348,12 @@ class TestCommand(DefaultCommand):
 		self.verbose = args.verbose
 		self.debug = args.debug
 
+# ---------------------------------------------------------------------------------------------------------------------
+class ConfigGenerationCommand(object):
+	def __call__(self, args):
+		config.write_default_config_file()
 
-def extract_file_name(content_dispo):
-	file_name = ""
-	for key_val in content_dispo.split(';') :
-		param = key_val.strip().split('=')
-		if param[0] == "filename":
-			file_name =  param[1]
-		if param[0] == "filename*":
-			file_name =  param[1].split("'")[2]
-	return file_name
-
-
-class file_with_callback(file):
-	def __init__(self, path, mode, callback, size=None, *args):
-		file.__init__(self, path, mode)
-		if size :
-			self._total = size
-		else:
-			self.seek(0, os.SEEK_END)
-			self._total = self.tell()
-		self.seek(0)
-		self._callback = callback
-		self._args = args
-		self._seen = 0.0
-		
-	def __len__(self):
-		return self._total
-	
-	def read(self, size):
-		data = file.read(self, size)
-		self._seen += size
-		if self._seen > self._total :
-			self._callback(self._total)
-		else:
-			self._callback(self._seen)
-		return data
-
-	def write(self, data):
-		if data :
-			self._seen += len(data)
-		if self._seen > self._total :
-			self._callback(self._total)
-		else:
-			self._callback(self._seen)
-		data = file.write(self, data)
-
-		
-			
+# ---------------------------------------------------------------------------------------------------------------------
 class UploadFileCommand(DefaultCommand):
 
 	def __call__(self, args):
@@ -356,7 +364,7 @@ class UploadFileCommand(DefaultCommand):
 
 	def uploadFile(self, file_name):
 		""" upload a file to LinShare using its rest api. return the uploaded document uuid  """
-		url = self.root_url + self.fragment_upload + ".json"
+		url = self.root_url + "documents.json"
 		self.log.debug("upload url : "+ url)
 
 		# Generating datas and headers
@@ -394,6 +402,7 @@ class UploadFileCommand(DefaultCommand):
 
 
 
+# ---------------------------------------------------------------------------------------------------------------------
 class DownloadFileCommand(DefaultCommand):
 
 	def __call__(self, args):
@@ -405,7 +414,7 @@ class DownloadFileCommand(DefaultCommand):
 
 	def downloadFile(self, file_uuid):
 		""" download a file from LinShare using its rest api. """
-		url = self.root_url + self.fragment_download % file_uuid
+		url = self.root_url + "documents/%s/download" % file_uuid
 		self.log.debug("download url : "+ url)
 
 		
@@ -472,17 +481,18 @@ class DownloadFileCommand(DefaultCommand):
 		self.log.info("The file '" + file_name  + "' was downloaded. (" + req_time + "s)")
 
 
-class ListFileCommand(DefaultCommand):
+# ---------------------------------------------------------------------------------------------------------------------
+class ListDocumentCommand(DefaultCommand):
 
 	def __call__(self, args):
-		super(ListFileCommand, self).__call__(args)
+		super(ListDocumentCommand, self).__call__(args)
 		#self.log.debug(str(self.__class__))
 
 		self.listFile()
 
 	def listFile(self):
-		""" upload a file to LinShare using its rest api. return the uploaded document uuid  """
-		url = self.root_url + self.fragment_list + ".json"
+		""" List all documents store into LinShare."""
+		url = self.root_url + "documents.json"
 		self.log.debug("list url : "+ url)
 
 
@@ -510,6 +520,45 @@ class ListFileCommand(DefaultCommand):
 			print "%(name)-60s\t %(uuid)s" % f
 			#print "File names : %(name)-60s\t %(uuid)s" % f
 
+# ---------------------------------------------------------------------------------------------------------------------
+class ListReceivedShareCommand(DefaultCommand):
+
+	def __call__(self, args):
+		super(ListReceivedShareCommand, self).__call__(args)
+		#self.log.debug(str(self.__class__))
+		self.listReceivedShares()
+
+	def listReceivedShares(self):
+		"""List received shares store in linshare"""
+		url = self.root_url + "shares.json"
+		self.log.debug("list url : "+ url)
+
+
+		# Building request
+		request = urllib2.Request(url)
+
+		# request start
+		starttime =datetime.now()
+
+		# doRequest
+		resultq = urllib2.urlopen(request)
+
+		# request end
+		endtime = datetime.now()
+		result = resultq.read()
+
+		self.log.debug("the result is : " + str(result))
+		jObj = json.loads(result)
+
+		req_time = str(endtime - starttime)
+	#	self.log.info("The file '" + jObj.get('name') + "' ("+ jObj.get('uuid') + ") was uploaded. (" + req_time + "s)")
+		print "Received Shares: "
+		print "------------"
+		for f in jObj:
+			print "%(name)-60s\t %(uuid)s" % f
+			#print "File names : %(name)-60s\t %(uuid)s" % f
+
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 ####################################################################################
@@ -523,15 +572,6 @@ parser.add_argument('-V', '--verbose', action="store_true", default=False)
 parser.add_argument('-D', '--debug', action="store_true", default=False, help=argparse.SUPPRESS)
 parser.add_argument('-c', '--config', action="store", help="other configuration file.")
 parser.add_argument('-s', dest='server_section', action="store", help="This option let you select the server section in the ini file you want to load (server section is always load first as default configuration). You just need to specify a number like '4' for section 'server-4'.")
-
-#parser.add_argument('-u', '--user', action="store", default=config.linshare.user, dest="user")
-##, required=True)
-#parser.add_argument('-p', '--password', action="store", default=config.linshare.password)
-#parser.add_argument('-H', '--host', action="store", default=config.linshare.host)
-#parser.add_argument('-a', '--appname', action="store", default=config.linshare.application_name, dest="application_name")
-#parser.add_argument('-r', '--realm', action="store", help=argparse.SUPPRESS, default=config.linshare.realm)
-
-
 parser.add_argument('-u', '--user', action="store", dest="user")
 #, required=True)
 parser.add_argument('-p', '--password', action="store")
@@ -544,39 +584,76 @@ subparsers = parser.add_subparsers()
 
 
 ####################################################################################
-### document
+### documents
 ####################################################################################
 def add_download_parser(subparsers, name, desc):
 	parser_tmp = subparsers.add_parser(name, help=desc)
 
 	subparsers2 = parser_tmp.add_subparsers()
-	parser_tmp2 = subparsers2.add_parser('upload', help="upload files to linshare")
+
+	parser_tmp2 = subparsers2.add_parser('upload', help="upload documents to linshare")
 	parser_tmp2.set_defaults(__func__=UploadFileCommand())
 	parser_tmp2.add_argument('-f', '--file', action="append", dest="files", required=True)
 
 
-	parser_tmp2 = subparsers2.add_parser('download', help="download files from linshare")
+	parser_tmp2 = subparsers2.add_parser('download', help="download documents from linshare")
 	parser_tmp2.set_defaults(__func__=DownloadFileCommand())
 	parser_tmp2.add_argument('-f', '--file', action="append", dest="files", required=True)
 
-	parser_tmp2 = subparsers2.add_parser('list', help="list files from linshare")
-	parser_tmp2.set_defaults(__func__=ListFileCommand())
+	parser_tmp2 = subparsers2.add_parser('list', help="list documents from linshare")
+	parser_tmp2.set_defaults(__func__=ListDocumentCommand())
 
+####################################################################################
+### shares
+####################################################################################
+def add_share_parser(subparsers, name, desc):
+	parser_tmp = subparsers.add_parser(name, help=desc)
 
-add_download_parser(subparsers, "document", "document tools")
-
-
-#parser_tmp = subparsers.add_parser('date',  help="Find new aired episodes.")
-#parser_tmp.set_defaults(__func__=SearchForNewEpisodeCommand())
-#parser_tmp.add_argument('-t', '--root-directory', action="store", default=config.root_directory)
-#parser_tmp.add_argument('-d', '--directory', action="store")
-#parser_tmp.add_argument('-s', '--season', action="store", required=True, type=int)
-#parser_tmp.add_argument('-e', '--episode', action="store", default=0, type=int)
+#	subparsers2 = parser_tmp.add_subparsers()
+#
+#	parser_tmp2 = subparsers2.add_parser('create', help="upload files to linshare")
+#	parser_tmp2.set_defaults(__func__=UploadFileCommand())
+#	parser_tmp2.add_argument('-f', '--file', action="append", dest="files", required=True)
+#
+#	parser_tmp2 = subparsers2.add_parser('list', help="list files from linshare")
+#	parser_tmp2.set_defaults(__func__=ListShareCommand())
 
 
 ####################################################################################
-### TEST
+### received shares
 ####################################################################################
+def add_share_parser(subparsers, name, desc):
+	parser_tmp = subparsers.add_parser(name, help=desc)
+
+	subparsers2 = parser_tmp.add_subparsers()
+
+	#parser_tmp2 = subparsers2.add_parser('create', help="upload files to linshare")
+	#parser_tmp2.set_defaults(__func__=UploadFileCommand())
+	#parser_tmp2.add_argument('-f', '--file', action="append", dest="files", required=True)
+
+	parser_tmp2 = subparsers2.add_parser('list', help="list received shares from linshare")
+	parser_tmp2.set_defaults(__func__=ListReceivedShareCommand())
+
+####################################################################################
+### config 
+####################################################################################
+
+def add_config_parser(subparsers, name, desc):
+	parser_tmp = subparsers.add_parser(name, help=desc)
+
+	subparsers2 = parser_tmp.add_subparsers()
+
+	parser_tmp2 = subparsers2.add_parser('generate', help="generate the default pref file")
+	parser_tmp2.set_defaults(__func__=ConfigGenerationCommand())
+
+####################################################################################
+### Adding config parsers
+####################################################################################
+
+add_download_parser(subparsers, "document", "document management")
+add_share_parser(subparsers, "share", "share management")
+add_config_parser(subparsers, "config",  "config part")
+
 parser_tmp = subparsers.add_parser('test', add_help=False)
 parser_tmp.set_defaults(__func__=TestCommand())
 
@@ -596,17 +673,16 @@ if __name__ == "__main__" :
 		log.setLevel(logging.DEBUG)
 		streamHandler.setFormatter(myDebugFormat)
 		print config
-		#print args
 		print "----------- processing ----------------"
-
 
 		# run command
 		args.__func__(args)
 	else:
-		# run command
 		try:
+			# run command
 			args.__func__(args)
 		except Exception as a :
 			log.error("unexcepted error : " + str(a))
+			sys.exit(1)
 
 sys.exit(0)
