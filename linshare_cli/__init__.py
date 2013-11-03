46fd3c81671186778b61fd7f7f844fc597bb84c5
Frédéric MARTIN <frederic.martin.fma@gmail.com>
2013-11-03 23:05:12 +0100

    Licence terms added.

--- linshare_cli/__init__.py
+++ linshare_cli/__init__.py
@@ -3,19 +3,21 @@
 
 
 # This file is part of Linshare user cli.
-# 
+#
 # LinShare user cli is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # LinShare user cli is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with LinShare user cli.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2013 Frédéric MARTIN
 #
 # Contributors list :
 #
