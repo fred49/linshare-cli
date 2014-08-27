2b63516f71d82dddf04297e9238c316c4edee24e
Frédéric MARTIN <frederic.martin.fma@gmail.com>
2014-08-28 00:50:48 +0200

    update licence

--- linshare-api/linshare_api/core.py
+++ linshare-api/linshare_api/core.py
@@ -2,22 +2,22 @@
 # -*- coding: utf-8 -*-
 
 
-# This file is part of Linshare user cli.
+# This file is part of Linshare api.
 #
-# LinShare user cli is free software: you can redistribute it and/or modify
+# LinShare api is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# LinShare user cli is distributed in the hope that it will be useful,
+# LinShare api is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with LinShare user cli.  If not, see <http://www.gnu.org/licenses/>.
+# along with LinShare api.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright 2013 Frédéric MARTIN
+# Copyright 2014 Frédéric MARTIN
 #
 # Contributors list :
 #
