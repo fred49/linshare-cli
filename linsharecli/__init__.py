cf11588ce426d0fc344a7db9d715516c6b4eca39
Frédéric MARTIN <frederic.martin.fma@gmail.com>
2016-09-27 23:44:12 +0200

    bump version up to 0.2.12

--- linsharecli/__init__.py
+++ linsharecli/__init__.py
@@ -24,4 +24,4 @@
 #  Frédéric MARTIN frederic.martin.fma@gmail.com
 #
 
-__version__ = '0.2.11'
+__version__ = '0.2.12'
--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed.
     install_requires = ['argtoolbox>=0.1.0',
-                        'linshareapi>=0.2.8',
+                        'linshareapi>=0.2.9',
                         'poster',
                         'progressbar',
                         'veryprettytable',
