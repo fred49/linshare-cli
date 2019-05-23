47e93e969affdaeea8af129656fa756ff2f06b3f
Frédéric MARTIN <frederic.martin.fma@gmail.com>
2019-05-23 18:31:35 +0200

    rshares commands are now using TableBuilder.

--- linsharecli/user/rshares.py
+++ linsharecli/user/rshares.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+"""TODO"""
 
 
 # This file is part of Linshare cli.
@@ -33,26 +34,12 @@ from linsharecli.common.core import add_delete_parser_options
 from linsharecli.common.core import add_download_parser_options
 from linsharecli.common.filters import PartialOr
 from linsharecli.common.filters import PartialDate
-from linsharecli.common.formatters import DateFormatter
-from linsharecli.common.formatters import SizeFormatter
+from linsharecli.common.tables import DownloadAction
+from linsharecli.common.tables import DeleteAction
+from linsharecli.common.tables import TableBuilder
 
 class ReceivedSharesCommand(DefaultCommand):
     """ List all received shares"""
-
-    DEFAULT_TOTAL = "Received shares found : %(count)s"
-    MSG_RS_NOT_FOUND = "No received shares could be found."
-    MSG_RS_DELETED = "The received share '%(name)s' (%(uuid)s) was deleted. (%(time)s s)"
-    MSG_RS_CAN_NOT_BE_DELETED = "The received share '%(uuid)s' can not be deleted."
-    MSG_RS_CAN_NOT_BE_DELETED_M = "%(count)s received share(s) can not be deleted."
-    MSG_RS_DOWNLOADED = "%(position)s/%(count)s: The received share '%(name)s' (%(uuid)s) was downloaded. (%(time)s s)"
-    MSG_RS_CAN_NOT_BE_DOWNLOADED = "One received share can not be downloaded."
-    MSG_RS_CAN_NOT_BE_DOWNLOADED_M = "%(count)s received shares can not be downloaded."
-
-    ACTIONS = {
-        'delete' : '_delete_all',
-        'download' : '_download_all',
-        'count_only' : '_count_only',
-    }
 
     def complete(self, args, prefix):
         super(ReceivedSharesCommand, self).__call__(args)
@@ -67,48 +54,48 @@ class ReceivedSharesListCommand(ReceivedSharesCommand):
     @Time('linsharecli.rshares', label='Global time : %(time)s')
     def __call__(self, args):
         super(ReceivedSharesListCommand, self).__call__(args)
-        cli = self.ls.rshares
-        table = self.get_table(args, cli, self.IDENTIFIER, args.fields)
-        json_obj = cli.list()
-        # Filters
-        filters = [
+        endpoint = self.ls.rshares
+        tbu = TableBuilder(self.ls, endpoint, self.IDENTIFIER)
+        tbu.load_args(args)
+        tbu.add_filters(
             PartialOr(self.IDENTIFIER, args.names, True),
             PartialDate("creationDate", args.cdate)
-        ]
-        # Formatters
-        formatters = [
-            DateFormatter('creationDate'),
-            DateFormatter('expirationDate'),
-            SizeFormatter('size'),
-            DateFormatter('modificationDate')
-        ]
-        return self._list(args, cli, table, json_obj, formatters, filters)
+        )
+        return tbu.build().load_v2(endpoint.list()).render()
+
 
     def complete_fields(self, args, prefix):
+        """TODO"""
+        # pylint: disable=unused-argument
         super(ReceivedSharesListCommand, self).__call__(args)
         cli = self.ls.rshares
         return cli.get_rbu().get_keys(True)
 
 
 class ReceivedSharesDownloadCommand(ReceivedSharesCommand):
+    """TODO"""
 
     @Time('linsharecli.rshares', label='Global time : %(time)s')
     def __call__(self, args):
         super(ReceivedSharesDownloadCommand, self).__call__(args)
-        cli = self.ls.rshares
-        return self._download_all(args, cli, args.uuids)
+        act = DownloadAction()
+        act.init(args, self.ls, self.ls.rshares)
+        return act.download(args.uuids)
 
 
 class ReceivedSharesDeleteCommand(ReceivedSharesCommand):
+    """TODO"""
 
     @Time('linsharecli.document', label='Global time : %(time)s')
     def __call__(self, args):
         super(ReceivedSharesDeleteCommand, self).__call__(args)
-        cli = self.ls.rshares
-        return self._delete_all(args, cli, args.uuids)
+        act = DeleteAction()
+        act.init(args, self.ls, self.ls.rshares)
+        return act.delete(args.uuids)
 
 
 def add_parser(subparsers, name, desc, config):
+    """TODO"""
     parser_tmp = subparsers.add_parser(name, help=desc)
 
     subparsers2 = parser_tmp.add_subparsers()
